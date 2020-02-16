885fed6e8e297665dd002375a576068b8b013db7
Vipan K Singh <vksinghh@Vipans-MacBook-Pro.local>
2020-02-16 15:41:24 +0530

    adding another unit test

--- mult.test.js
+++ mult.test.js
@@ -1,5 +1,9 @@
 const mult = require('./mult');
 
+test('mult 10 and 10 to get 100', () => {
+	expect(mult(10, 10)).toBe(100);
+});
+
 test('mult anything to 0 is 0', () => {
 	expect(mult(10, 0)).toBe(0);
 });
